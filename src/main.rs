475d46a237c4b35a7fdf2d1d37e401749ea59218
sejin-P <chunpark37@gmail.com>
2023-09-24 15:41:45 +0900

    fix; disabling unwinding && start attribute

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 #![no_std]
-fn main() {
+// To tell the Rust compiler that we don’t want to use the normal entry point chain, we add the #![no_main] attribute.
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// returning ! means it's diverging func.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+
+// main doesn’t make sense without an underlying runtime that calls it.
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+
