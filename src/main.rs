0880885aab627d0afb91fdecae03a83ffa4860ff
sejin-P <chunpark37@gmail.com>
2023-10-07 19:56:53 +0900

    feat; implement print using vfa

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,21 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 // main doesn’t make sense without an underlying runtime that calls it.
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // start address of buffer
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        // why unsafe? -> rust compiler doesn't know whether its pointer is valid or not
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
